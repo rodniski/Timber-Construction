dc2c433d73e3fc0c17671437efe29c42df38ae19
Guilherme Rodniski Correia <guilherme.correia@rodoparana.com.br>
2024-03-11 14:34:54 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,9 @@ const App = () => {
         <Navbar/>
         <Hero/>
       </section>
-      <section>Hero</section>
-      <section id="Services">Parallax</section>
+      <section id="Services"><Parallax type="services"/></section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Portfolio"><Parallax type="portfolio"/></section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,25 +1,81 @@
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2> CONSTRUA O FUTURO!</h2>
-          <h1>
-            Transforme cada projeto com a potência incomparável dos maquinários
-            SANY
-          </h1>
-          <div className="buttons">
-            <button>Explore as opções</button>
-            <button>Fale conosco</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>CONSTRUA COM CONFIANÇA</motion.h2>
+          <motion.h1 variants={textVariants}>
+            SANY, a excelência em maquinários para a construção civil
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              Veja nossos Produtos
+            </motion.button>
+            <motion.button variants={textVariants}>
+              Entre em Contato
+            </motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
       </div>
-      <div className="slidingTextContainer">
-        TIMBER SANY FUCHS
-      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        SANY - TIMBER
+      </motion.div>
+
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
       </div>
