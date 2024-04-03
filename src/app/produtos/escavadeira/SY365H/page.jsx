a03a6a1b8b21ddac255f779e9a566b9fb1269806
Guilherme Rodniski Correia <guilherme.correia@rodoparana.com.br>
2024-04-03 09:31:07 -0300

    data

--- src/app/produtos/escavadeira/SY365H/page.jsx
+++ src/app/produtos/escavadeira/SY365H/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Product from "@components/Produtos/Product"; // Importando o componente ProductPage
-import SY365H from "@/data/produtos/escavadeira/sy365h/SY365H"; // Importando as informações específicas do produto SY135C
+import SY365H from "@data/produtos/escavadeira/sy365h/SY365H"; // Importando as informações específicas do produto SY135C
 function ProductPage() {
   return (
     <Product product={SY365H} />
--- src/app/produtos/escavadeira/SY500H/page.jsx
+++ src/app/produtos/escavadeira/SY500H/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Product from "@components/Produtos/Product"; // Importando o componente ProductPage
-import SY500H from "@/data/produtos/escavadeira/sy500h/SY500H"; // Importando as informações específicas do produto SY135C
+import SY500H from "@data/produtos/escavadeira/sy500h/SY500H"; // Importando as informações específicas do produto SY135C
 function ProductPage() {
   return (
     <Product product={SY500H} />
