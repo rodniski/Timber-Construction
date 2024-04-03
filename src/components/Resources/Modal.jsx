20f1da8034a0f7f92154bbd811ef8c38bc9d1dcd
Guilherme Rodniski Correia <guilherme.correia@rodoparana.com.br>
2024-04-03 13:14:48 -0300

    x modal

--- src/components/Resources/Modal.jsx
+++ src/components/Resources/Modal.jsx
@@ -1,11 +1,11 @@
 "use client";
-
 import React from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import ContactForm from "../ContactForm";
 
 const Modal = ({ onClose, productName }) => {
   const produto = productName;
+
   return (
     <Transition.Root show={true} as={React.Fragment}>
       <Dialog
@@ -23,10 +23,33 @@ const Modal = ({ onClose, productName }) => {
           leaveTo="opacity-0 scale-95"
         >
           <div className="flex items-center justify-center min-h-screen">
-            <div className="bg-white px-12 py-8 rounded-lg overflow-hidden shadow-xl transition-all">
-              <h2 className="text-3xl font-bold text-red-600 mb-8">
-                Entre em Contato sobre {produto}
-              </h2>
+            <div className="bg-white px-12 py-8 rounded-lg overflow-hidden shadow-xl transition-all relative">
+              <button
+                onClick={onClose}
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+
+              <div className="flex items-center justify-between mb-8">
+                <h2 className="text-3xl font-bold text-red-600">
+                  Entre em Contato sobre {produto}
+                </h2>
+                {/* Aqui você pode adicionar mais elementos ao lado do título */}
+              </div>
 
               <ContactForm productName={produto} />
             </div>
